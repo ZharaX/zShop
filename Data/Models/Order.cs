9186a8036e57acd531537c0518c3d859fbaef74d
Jens Burmeister <zharax@live.com>
2021-10-07 00:16:25 +0200

    CREATED: Interface Implementation for Customers, Products and Orders MOVED: DataAnnotations from classes to Fluid API CREATED: Summarys UPDATED: Model Class Diagram

--- Data/Models/Order.cs
+++ Data/Models/Order.cs
@@ -2,6 +2,7 @@
 {
 	/// <summary>
 	/// Context Class Defining an Order
+	/// NOTE: ANNOTATIONS ARE DEFINED BY FLUID API
 	/// </summary>
 	public class Order
 	{
@@ -10,5 +11,16 @@
 		public int Amount { get; set; }
 		public decimal TotalPrice { get; set; }
 		public decimal Discount { get; set; }
+
+		public DateTime Date { get; set; }
+		public DateTime CompletedOn { get; set; }
+		public bool IsProcessed { get; set; }
+
+		// CUSTOMER NAVIGATION REFERENCE
+		public int CustomerID { get; set; }
+		public Customer Customer { get; set; }
+
+		// PRODUCTS COLLECTION NAVIGATION FOR THIS ORDER
+		public ICollection<Product> Products { get; set; }
 	}
 }
--- Data/Models/Product.cs
+++ Data/Models/Product.cs
@@ -1,17 +1,26 @@
-﻿namespace Data.Models
+﻿using System.Collections.Generic;
+
+namespace Data.Models
 {
 	/// <summary>
 	/// Context Class Defining a Product
+	/// NOTE: ANNOTATIONS ARE DEFINED BY FLUID API
 	/// </summary>
 	public class Product
 	{
 		public int ProductID { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
-		public Category Category { get; set; }
 		public decimal Price { get; set; }
 		public int Stock { get; set; }
 		public string Image { get; set; }
 		public bool Active { get; set; }
+
+		// CATEGORY REFERENCE NAVIGATION
+		public int CategoryID { get; set; }
+		public Category Category { get; set; }
+
+		// ORDER COLLECTION NAVIGATION FOR THIS PRODUCT
+		public ICollection<Order> Orders { get; set; }
 	}
 }
