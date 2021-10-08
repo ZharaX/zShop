00bfc6a45adaa6311d90a7033e6360fcbba26d0a
Jens Burmeister <zharax@live.com>
2021-10-08 02:10:02 +0200

    ADDED: CustomerDTO Class CREATED: Pre-Authentication Functionality CREATED: CRUD implemented functionality, only with parts of code done so far CREATED: CustomerHandle Class, this class will take care of authentication/auto-logouts for inactive users

---
+++ Service/Service/DTO/CustomerDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Service.DTO
+{
+	public class CustomerDTO
+	{
+		public string SID { get; private set; }
+
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Address { get; set; }
+		public string City { get; set; }
+		public string Postal { get; set; }
+		public string Country { get; set; }
+		public string Phone { get; set; }
+		public string Email { get; set; }
+	}
+}
--- Service/Service/SiteFunctions.cs
+++ Service/Service/SiteFunctions.cs
@@ -13,179 +13,112 @@
 		/// <param name="action">CRUD Enum Selector</param>
 		/// <param name="function">Action Enum Selector</param>
 		/// <param name="data">T DATA</param>
-		/// <param name="ownerID">Owner of Products</param>
-		/// <returns></returns>
-		object PerformAction<T>(ActionType action, FunctionName function, T data, int ownerID) where T : class;
-
-		/// <summary>
-		/// Method for Performing Site Wide Admin Functions
-		/// </summary>
-		/// <typeparam name="T">T for DATA</typeparam>
-		/// <param name="action">CRUD Enum Selector</param>
-		/// <param name="function">Action Enum Selector</param>
-		/// <param name="data">T DATA</param>
-		/// <param name="ownerID">Owner of Products</param>
-		/// <returns></returns>
-		object PerformAdminAction<T>(ActionType action, FunctionName function, T data, int ownerID) where T : class;
-
-		/// <summary>
-		/// Method for handling access to Site Repository Functions
-		/// </summary>
-		/// <returns>SiteRepository Class</returns>
-		ISiteRepository SiteRepository();
-
-		/// <summary>
-		/// Method for handling access to Site Products Functions
-		/// </summary>
-		/// <returns>Data.Products Class</returns>
-		Data.IProducts SiteProducts();
+		/// <returns>Objects of Various Types</returns>
+		object PerformAction<T>(ActionType action, FunctionName function, T data) where T : class;
 	}
 
 	public class SiteFunctions : ISiteFunctions
 	{
 		// REFERENCE FOR DB MANAGER
-		private Data.IDBManager _dbManager;
-
-		// INTERFACE - CLASS REFERENCES
-		ISiteRepository _siteRepository;
-		Data.IProducts _siteProducts;
+		private readonly Data.IDBHandler _dbContext;
+		private readonly ICustomerHandler _customerHandler;
 
 		/// <summary>
-		/// Constructor: Setting class references on instantiation (Startup)
+		/// Constructor: Supplying ConnectionString for DBContext Use
+		/// Instantiates: DataTransferObjects Class
 		/// </summary>
 		/// <param name="connectionString">DB CONNECTION STRING</param>
 		public SiteFunctions(string connectionString)
 		{
 			// CREATE DB MANAGER REFERENCE
-			_dbManager = new Data.DBManager(connectionString);
-			// PASSING 'IDBManager' REFERENCE
-			_siteRepository = new SiteRepository(_dbManager);
-			_siteProducts = new Data.Products(_dbManager);
+			_dbContext = new Data.ZShopContext(connectionString);
+			_customerHandler = new CustomerHandler(_dbContext);
 
-			// SETTING DTO HANDLER CLAS REFERENCE
-			DataTransferObjects.SiteRepository = _siteRepository;
+			// SETTING DTO HANDLER CLASS REFERENCE
+			//DataTransferObjects.SiteRepository = _siteRepository;
 		}
-
-		public ISiteRepository SiteRepository() { return _siteRepository; }
-		public Data.IProducts SiteProducts() { return _siteProducts; }
 
 		#region NORMAL USER FUNCTION HANDLER
 		/// <summary>
 		/// Method handling retrieval only of various requested data.
 		/// Create, Update & Delete will be an Admin only feature.
 		/// Action: Retrieve
-		/// Function: Location, Product etc.
+		/// Function: Customer, Order, Product etc.
 		/// </summary>
 		/// <param name="action">Enum Values</param>
 		/// <param name="function">Enum Values</param>
 		/// <param name="data">Data to be Handled</param>
-		/// <param name="ownerID">Requesting Owner</param>
 		/// <returns>Object: Depending on Data</returns>
-		public object PerformAction<T>(ActionType action, FunctionName function, T data, int ownerID) where T : class
+		public object PerformAction<T>(ActionType action, FunctionName function, T data) where T : class
 		{
 			switch (action)
 			{
 				// CASE:
 				// ACTION -> CREATE
 				case ActionType.Create:
-					return "NOT ALLOWED"; // NOT ADMIN -> CANNOT USE CREATE
+					if (function == FunctionName.Customer) // FUNCTION: CUSTOMER
+						return _dbContext.CreateCustomer(data as Data.Models.Customer);
+
+					if (function == FunctionName.Order) // FUNCTION: PRODUCT
+						return null;
+
+					if (function == FunctionName.Product) // FUNCTION: PRODUCT
+						return NewProduct(data);
+
+					if (function == FunctionName.Categorys) // FUNCTION: CATEGORY
+						return null;
+
+					return null; // WE SHOULD EVEN NOT BE GETTING HERE
 
 				// CASE:
 				// ACTION -> RETRIEVE
 				case ActionType.Retrieve:
-					if (function == FunctionName.StorageDataOwner) // FUNCTION: STORAGE
-						return _siteRepository.AddUserForSession(data as string[]); /*.DBGetStorageOwnerData(data as string); // TODO:*/
+					if (function == FunctionName.Customer) // FUNCTION: CUSTOMER
+						return _customerHandler.AddUserForSession(data as string[]);
 
-					if (function == FunctionName.StorageForOwner) // FUNCTION: LOCATION
-						return _dbManager.DBGetStorageForOwner(ownerID);
+					if (function == FunctionName.Order) // FUNCTION: ORDER
+						return _dbContext.GetOrder(data as int[]);
 
 					if (function == FunctionName.Product) // FUNCTION: PRODUCT
 						return ReturnProductData(data);
 
 					if (function == FunctionName.Categorys) // FUNCTION: CATEGORY
-						return _siteRepository.GetAllCategorys(ownerID);
-
-					if (function == FunctionName.Containers) // FUNCTION: CONTAINERS
-						return _dbManager.DBGetContainers(data as int[]);
-
-					if (function == FunctionName.Colors) // FUNCTION: COLORS
-						return _siteRepository.GetAllColors(ownerID);
+						return _dbContext.GetAllCategorys();
 
 					return null; // WE SHOULD EVEN NOT BE GETTING HERE
 
 				// CASE:
 				// ACTION -> UPDATE
 				case ActionType.Update:
-					return "NOT ALLOWED"; // NOT ADMIN -> CANNOT USE UPDATE
+					if (function == FunctionName.Customer) // FUNCTION: CUSTOMER
+						return null;
 
-				// CASE:
-				// ACTION -> DELETE
-				case ActionType.Delete:
-					return "NOT ALLOWED"; // NOT ADMIN -> CANNOT USE DELETE
-
-
-				default:
-					return null; // DEFAULTS NULL IF WE EVEN SHOULD BE GETTING HERE!
-			}
-		}
-		#endregion
-		#region ADMIN USER FUNCTION HANDLER
-		/// <summary>
-		/// Method handling all Admin functionality
-		/// Action: CRUD Model
-		/// Function: Location, Product etc.
-		/// </summary>
-		/// <param name="action">Enum Values</param>
-		/// <param name="function">Enum Values</param>
-		/// <param name="data">Data to be Handled</param>
-		/// <param name="ownerID">Requesting Owner</param>
-		/// <returns>Object: Depending on Data</returns>
-		public object PerformAdminAction<T>(ActionType action, FunctionName function, T data, int ownerID) where T : class
-		{
-			switch (action)
-			{
-				// CASE:
-				// ACTION -> CREATE
-				case ActionType.Create:
-					if (function == FunctionName.StorageDataOwner) // FUNCTION: NEW STORAGE OWNER
-					{
-						Data.Storage.StorageData owner = DataTransferObjects.Storage(data);
-						return _dbManager.DBCreateNewOwner(owner);
-					}
-
-					if (function == FunctionName.Location) // FUNCTION: LOCATION
-						return ""; // TODO: LATER FEATURE CREATION
-
-					if (function == FunctionName.Product) // FUNCTION: CREATE NEW PRODUCT
-						return NewProduct(data, ownerID);
-
-					if (function == FunctionName.Containers) // FUNCTION: CREATE NEW CONTAINER FOR ASSET
-						return _dbManager.DBCreateContainer(data as int[], ownerID);
-
-					if (function == FunctionName.NewLocationAsset) // FUNCTION: CREATE NEW ASSET FOR OWNER
-						return CreateAssets(data as int[], ownerID);
-
-					return null; // WE SHOULD EVEN NOT BE GETTING HERE
-
-				// CASE:
-				// ACTION -> RETRIEVE
-				case ActionType.Retrieve:
-					return null; // WE SHOULD EVEN NOT BE GETTING HERE
-
-				// CASE:
-				// ACTION -> UPDATE
-				case ActionType.Update:
-					if (function == FunctionName.ProductLocation) // FUNCTION: LOCATION
-						return UpdateProductLocation(data);
+					if (function == FunctionName.Order) // FUNCTION: ORDER
+						return null;
 
 					if (function == FunctionName.Product) // FUNCTION: PRODUCT
-						return _dbManager.DBUpdateProduct(DataTransferObjects.Product(data), ownerID);
+						return UpdateProduct(data);
+
+					if (function == FunctionName.Categorys) // FUNCTION: CATEGORY
+						return null;
 
 					return null; // WE SHOULD EVEN NOT BE GETTING HERE
 
 				// CASE:
 				// ACTION -> DELETE
 				case ActionType.Delete:
+					if (function == FunctionName.Customer) // FUNCTION: CUSTOMER
+						return null;
+
+					if (function == FunctionName.Order) // FUNCTION: ORDER
+						return null;
+
+					if (function == FunctionName.Product) // FUNCTION: PRODUCT
+						return null;
+
+					if (function == FunctionName.Categorys) // FUNCTION: CATEGORY
+						return null;
+
 					return null; // WE SHOULD EVEN NOT BE GETTING HERE
 
 
@@ -196,71 +129,45 @@
 		#endregion
 		#region PRIVATE METHODS FOR SITE ACTIONS
 		/// <summary>
-		/// Method for handling the creation of new Assets (Rooms, Shelves, Pallets...)
-		/// Also handles the correct DB Function based on incoming data
-		/// </summary>
-		/// <param name="assetData">The Assets Data</param>
-		/// <param name="ownerID">Owner of Asset</param>
-		/// <returns>DB Message</returns>
-		private string CreateAssets(int[] assetData, int ownerID)
-		{
-			if (assetData[0] == -1) // DIFFERENT CREATE METHOD
-				return _dbManager.DBCreateNewRoomAsset(assetData, ownerID);
-
-			// ALL OTHER ASSETS ARE HANDLED THE SAME
-			return _dbManager.DBCreateNewAsset(assetData, ownerID);
-		}
-
-		/// <summary>
 		/// Method Product Creator and Database Inserter
 		/// </summary>
-		/// <param name="product">The Product Data Object(UI)</param>
-		/// <param name="ownerID">Owner of Product</param>
-		/// <returns>DB Message</returns>
-		private string NewProduct<T>(T product, int ownerID) where T : class
+		/// <param name="data">The Product Data Object (UI)</param>
+		/// <returns>Success/Failure Status</returns>
+		private bool NewProduct<T>(T data)
 		{
-			// PRODUCT UI MODEL (UI) WILL BE 'CONVERTED' TO DATA.ProductData (DATA)
-			Data.Product.ProductData p = DataTransferObjects.Product(product);
+			// UI.PRODUCT WILL BE 'CONVERTED' TO DATA.MODELS.PRODUCT (DATA)
+			//Data.Models.Product p = DataTransferObjects.Product(data);
 
 			// PERFORMS PRODUCT CREATION -> RETURNS DB MESSAGE
-			return _siteProducts.AddNewProduct(p, _siteRepository.GetAllCategorys(ownerID).FindIndex(cID => cID.Contains(p.Category)), ownerID);
+			return false;
 		}
 
 		/// <summary>
-		/// Method for finding Product per Barcode
+		/// Method for finding Product per ID
 		/// </summary>
-		/// <param name="data">Barcode Sent</param>
-		/// <returns>Data.Product (or Not Found String)</returns>
+		/// <param name="data">ID</param>
+		/// <returns>Data.Models.Product or (Not Found String)</returns>
 		private object ReturnProductData<T>(T data)
 		{
-			Data.Product.ProductData p = _dbManager.DBGetProductByBarcode(data.ToString(), 1);
+			Data.Models.Product p = _dbContext.GetProduct(data as int[]);
 
-			if (p != null)
-			{
-				if (p.ID == 0 && p.Barcode == "SERIOUSLY?!")
-					return "ERROR!";
-				else
-					return p;
-			}
-			else
-				return "Vare Ikke Fundet!";
+			if (p != null) return p;
+			
+			return "Vare Ikke Fundet!";
 		}
 
 		/// <summary>
-		/// Method for Updating a Products Location
+		/// Method for Updating a Product
 		/// </summary>
-		/// <param name="data">data[0]: PID -> data[1]: LocationData</param>
-		/// <returns>Success/Fail Message</returns>
-		private string UpdateProductLocation<T>(T data)
+		/// <param name="data">T Product Model</param>
+		/// <returns>Success/Failure Status</returns>
+		private bool UpdateProduct<T>(T data)
 		{
-			// T 'data' IS OF TYPE ARRAY SO WE CAST IT AS SUCH
-			string[] t = data as string[];
-			// ID:0 -> PRODUCT ID
-			// ID:1 -> LOCATION DATA
-			return _dbManager.DBSetProductLocation(t[0].ToString(), t[1].ToString());
+			return _dbContext.UpdateProduct(data as Data.Models.Product);
 		}
 		#endregion
 	}
+
 	/// <summary>
 	/// CRUD IMPLEMENTER ENUM
 	/// </summary>
@@ -277,14 +184,9 @@
 	/// </summary>
 	public enum FunctionName
 	{
-		StorageDataOwner,
-		StorageForOwner,
-		Location,
+		Customer,
+		Order,
 		Product,
-		ProductLocation,
-		Categorys,
-		Colors,
-		Containers,
-		NewLocationAsset
+		Categorys
 	}
 }
