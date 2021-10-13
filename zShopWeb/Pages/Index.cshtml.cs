a717cdfc7c671a2e6fb53bbf5bfa926f718c4588
Jens Burmeister <zharax@live.com>
2021-10-14 01:46:15 +0200

    ADDED: Razor Web Core to Project

---
+++ zShopWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zShopWeb.Pages
+{
+	public class IndexModel : PageModel
+	{
+		private readonly Service.ISiteFunctions _sitefunctions;
+		private readonly ILogger<IndexModel> _logger;
+
+		public IndexModel(Service.ISiteFunctions siteFunctions, ILogger<IndexModel> logger)
+		{
+			_sitefunctions = siteFunctions;
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+			//var customers = _sitefunctions.PerformAction<IQueryable>(Service.ActionType.Query, Service.FunctionName.Customer, null);
+			//Service.DTO.CustomerDTO cust = (Service.DTO.CustomerDTO)_sitefunctions.PerformAction(Service.ActionType.Retrieve, Service.FunctionName.Customer, 1);
+
+			//cust.FirstName = "Jens";
+			//bool t = (bool)_sitefunctions.PerformAction(Service.ActionType.Update, Service.FunctionName.Customer, cust);
+		}
+	}
+}
---
+++ zShopWeb/Startup.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+namespace zShopWeb
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			string connectionString = Configuration.GetConnectionString("S21DMH3B11_zShopDBContext");
+			services.AddDbContext<Data.IDBManager, Data.ZShopContext>(
+				options => options.UseSqlServer(connectionString)
+			);
+
+			services.AddScoped<Service.ISiteFunctions, Service.SiteFunctions>();
+
+			services.AddRazorPages();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapRazorPages();
+			});
+		}
+	}
+}
