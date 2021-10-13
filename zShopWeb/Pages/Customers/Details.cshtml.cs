d46e81c04aaf51b5726e60d6229393e5ac1d7e2d
Jens Burmeister <zharax@live.com>
2021-10-14 01:49:32 +0200

    ADDED: CRUD Web Pages (Except Delete)

---
+++ zShopWeb/Pages/Customers/Details.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Service;
+
+namespace S21DMH3B11_zShop.Pages.Customer
+{
+	public class DetailsModel : PageModel
+	{
+		#region CONTEXT DATA
+		// ACCESS TO SERVICE LAYER AND FUNCTIONS
+		private readonly ISiteFunctions _siteFunctions;
+		#endregion
+
+		public DetailsModel(ISiteFunctions siteFunctions) { _siteFunctions = siteFunctions; }
+
+		public Service.DTO.CustomerDTO Customer { get; set; }
+
+		public async Task<IActionResult> OnGetAsync(int? id)
+		{
+			id = 1;
+			if (id == null) { return NotFound(); }
+
+			Customer = (Service.DTO.CustomerDTO)_siteFunctions.PerformAction(ActionType.Retrieve, FunctionName.Customer, id);
+
+			if (Customer == null) return NotFound();
+			return Page();
+		}
+	}
+}
---
+++ zShopWeb/Pages/Customers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Service;
+
+namespace S21DMH3B11_zShop.Pages.Customer
+{
+	public class IndexModel : PageModel
+	{
+		#region CONTEXT DATA
+		// ACCESS TO SERVICE LAYER AND FUNCTIONS
+		private readonly ISiteFunctions _siteFunctions;
+		#endregion
+
+		public IndexModel(ISiteFunctions siteFunctions) { _siteFunctions = siteFunctions; }
+
+		public IList<Service.DTO.CustomerDTO> Customers { get; set; }
+
+		
+		public async Task OnGetAsync()
+		{
+			Customers = (List<Service.DTO.CustomerDTO>)_siteFunctions.PerformAction<List<Service.DTO.CustomerDTO>>(ActionType.Query, FunctionName.Customer, null);
+		}
+	}
+}
